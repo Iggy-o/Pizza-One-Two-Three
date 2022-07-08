b06f1bd8b839341bc5273132dc3d3616d90ddaef
William-Qu <95053253+William-Qu@users.noreply.github.com>
2022-07-08 16:49:50 -0400

    STEM REPO

--- Assets/Scripts/toolControler.cs
+++ Assets/Scripts/toolControler.cs
@@ -7,13 +7,18 @@ public class toolControler : MonoBehaviour
 
     public Texture2D cursorHandPic;
     public Texture2D cursorForkPic;
-
     
+    //new added stuff
+    SpriteRenderer sprite;
+    public GameObject othersprite;
+
+
     public KeyCode mouseLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //new added stuff
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,11 +33,19 @@ public class toolControler : MonoBehaviour
         {
             PaintPizza.toolType = "eraser";
             Cursor.SetCursor(cursorForkPic, Vector2.zero, CursorMode.ForceSoftware);
+
+            //new added stuff
+            sprite.color = new Color(1, 1, 0, 1);
+            othersprite.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 1);
         }
         if (gameObject.name == "hand")
         {
             PaintPizza.toolType = "addStuff";
             Cursor.SetCursor(cursorHandPic, Vector2.zero, CursorMode.ForceSoftware);
+
+            //new added stuff
+            sprite.color = new Color(1, 1, 0, 1);
+            othersprite.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 1);
         }
     }
 }
